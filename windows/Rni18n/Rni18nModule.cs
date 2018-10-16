a966e6ce165a6c88f2717b88a548d6a09ff87b31
newomble <nwomble@capstone-is.com>
2018-10-16 14:46:46 -0400

    weak windows support

---
+++ windows/Rni18n/Rni18nModule.cs
@@ -0,0 +1,59 @@
+using ReactNative.Bridge;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace Rni18n.Rni18n
+{
+    /// <summary>
+    /// A module that allows JS to share data.
+    /// </summary>
+    class Rni18nModule : NativeModuleBase
+    {
+        /// <summary>
+        /// Instantiates the <see cref="Rni18nModule"/>.
+        /// </summary>
+        internal Rni18nModule()
+        {
+
+        }
+
+        /// <summary>
+        /// The name of the native module.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return "RNI18n";
+            }
+        }
+
+        private ArrayList GetLocaleList()
+        {
+            var returnList = new ArrayList();
+            var langList = Windows.System.UserProfile.GlobalizationPreferences.Languages;
+            var langListEnum = langList.GetEnumerator();
+            while (langListEnum.MoveNext())
+            {
+                var curr = langListEnum.Current;
+                returnList.Add(curr);
+            }
+            return returnList;
+        }
+
+        [ReactMethod]
+        async void getLanguages(IPromise promise)
+        {
+            try
+            {
+                promise.Resolve(GetLocaleList());
+            }
+            catch (Exception e)
+            {
+                promise.Reject(e);
+            }
+        }
+    }
+}
